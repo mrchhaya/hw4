c5e6a51b6a32455b15e30a40bc59a5af9af4ae48
Mohit Chhaya <mohitchhaya24@gmail.com>
2023-11-01 03:04:30 -0500

    hw4

---
+++ problem 2/dynamic/simple-typed-dynamic-kompiled/configVars.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_PGM='Stmt'
+declaredConfigVar_IO='String'
+declaredConfigVar_STDIN='String'
+declaredConfigVar_IO='String'
+declaredConfigVars=(
+    'PGM'
+    'IO'
+    'STDIN'
+    'IO'
+)
---
+++ problem 4/simple-typed-static-kompiled/configVars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_PGM='Stmt'
+declaredConfigVars=(
+    'PGM'
+)
